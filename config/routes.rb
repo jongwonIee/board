e09b15534a7a56fa4d55348d9c60173ff61fd95c
jongwonIee <ljw3728@gmail.com>
2017-04-04 20:08:28 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  #resources 'posts'
+  root 'posts#index'
+
+  get    '/posts'  => 'posts#index'
+  post  '/posts' =>   'posts#create'
+
+  get    '/posts/new' =>  'posts#new', as: 'new_post'
+
+  get    '/posts/:id'   =>   'posts#show', as: 'post'
+  get    '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+
+  patch  '/posts/:id'   =>   'posts#update'
+  delete '/posts/:id'   =>   'posts#destroy'
+  # put    '/posts/:id'   =>   'posts#update'
+
+end
