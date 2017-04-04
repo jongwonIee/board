fabded55bb86d84828a127e484f9967cfa086d2c
jongwonIee <ljw3728@gmail.com>
2017-04-04 21:08:25 +0900

    category model and model relationship

--- db/migrate/20170404093429_create_posts.rb
+++ db/migrate/20170404093429_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
 
+      t.integer :category_id
       t.string :title
       t.text :content
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404093429) do
+ActiveRecord::Schema.define(version: 20170404111751) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "en_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "category_id"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
